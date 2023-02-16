cbc443ea829798cd325b4875b65e35471e05398b
Rostave <80945696+Rostave@users.noreply.github.com>
2023-02-16 20:43:25 +0800

    +符文类Spell和符文卷轴类SpellScroll +字符串哈希储存静态类StrHas

---
+++ Assets/Script/SpellRel/Spell.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace R0.SpellRel
+{
+
+    /// <summary>
+    /// 符文类型枚举
+    /// </summary>
+    public enum SpellType
+    {
+        
+    }
+    
+    /// <summary>
+    /// 基础符文类
+    /// </summary>
+    public class Spell
+    {
+        public SpellType Type;
+        
+    }
+}
---
+++ Assets/Script/SpellRel/SpellScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using R0.Static;
+
+namespace R0.SpellRel
+{
+    /// <summary>
+    /// 符文卷轴类
+    /// </summary>
+    public class SpellScroll
+    {
+        
+        private List<Spell> _spells;
+
+        /// <summary>
+        /// 添加spell至符文列表
+        /// </summary>
+        /// <param name="spell">添加的符文</param>
+        public void AppendSpell(Spell spell)
+        {
+            
+        }
+    }
+}
